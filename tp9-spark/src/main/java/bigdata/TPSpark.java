2e764acc8897611313dbde2ea516b14857cb3ab4
Jeremy Sabourin <jsabourin001@emi.u-bordeaux1.fr>
2018-01-24 16:28:43 +0100

    ajout de filter et de map

--- tp9-spark/src/main/java/bigdata/TPSpark.java
+++ tp9-spark/src/main/java/bigdata/TPSpark.java
@@ -1,7 +1,5 @@
 package bigdata;
 
-import java.util.Arrays;
-
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -9,17 +7,27 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class TPSpark {
 
 	public static void main(String[] args) {
-		
 		SparkConf conf = new SparkConf().setAppName("TP Spark");
 		JavaSparkContext context = new JavaSparkContext(conf);
-		JavaRDD<String> cities = context.textFile(args[1]);
-		JavaRDD<Integer> pop = cities.map(line ->{
-			String[] tmp=line.split(",");
-			return Integer.parseInt(tmp[4]);
-		});
-		pop.saveAsTextFile(args[2]);
+		JavaRDD<String> cities = context
+				.textFile(args[0])
+				.filter(line -> { 
+					if(line.split(",")[4].isEmpty() ||
+							line.split(",")[5].isEmpty() ||
+							line.split(",")[6].isEmpty() ||
+							line.split(",")[4].compareTo("Population")==0) 
+						return false; 
+					return true;
+				})
+				.map(res ->{
+						String[] tmp=res.split(",");
+						return tmp[4]+","+tmp[5]+","+tmp[6];
+				});
 		
+		cities.saveAsTextFile(args[1]);
+		context.close();
 		
 	}
 	
 }
+
